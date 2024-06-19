7e2c6e8d36e927ef41e42f721c7a270c1d2bde92
Maarten Postma <maarten.postma1000@gmail.com>
2024-06-19 10:20:38 +0200

    Adding all field required for the 2 classes

---
+++ src/main/java/nl/novi/Person.java
@@ -0,0 +1,20 @@
+package nl.novi;
+
+import java.util.List;
+
+public class Person {
+
+  private String name;
+  private String middleName;
+  private String lastName;
+  private String sex;
+  private int age;
+  private Person mother;
+  private Person father;
+  private List<Person> siblings;
+  private List<Person> children;
+  private List<Pet> pets;
+
+
+
+}
---
+++ src/main/java/nl/novi/Pet.java
@@ -0,0 +1,10 @@
+package nl.novi;
+
+public class Pet {
+
+  private String name;
+  private int age;
+  private String species;
+  private Person owner;
+
+}
