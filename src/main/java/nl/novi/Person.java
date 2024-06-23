115f96342d25b87a5c1cfa024292f7c45d6580f2
Maarten Postma <maarten.postma1000@gmail.com>
2024-06-24 00:09:52 +0200

    Add canGetFirstName unit test

--- src/main/java/nl/novi/Person.java
+++ src/main/java/nl/novi/Person.java
@@ -63,5 +63,4 @@ public class Person {
     }
     return grandChildren;
   }
-
 }
--- src/test/java/nl/novi/PersonTest.java
+++ src/test/java/nl/novi/PersonTest.java
@@ -3,65 +3,29 @@ package nl.novi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PersonTest {
+
+  private Person person;
+  private Person personWithMiddleName;
 
   @BeforeEach
   void setUp() {
-    Person person = new Person("Maarten" , "Postma", "male", 33);
+    person = new Person("Maarten", "Postma", "male", 33);
+    personWithMiddleName = new Person("John", "Middle", "Doe", "male", 33);
   }
 
   @Test
-  void addParents() {
+  void canGetFirstName() {
+    // arrange
+    var firstName = "Maarten";
+    // act
+    person.setName(firstName);
+    personWithMiddleName.setName(firstName);
+    // assert
+    assertEquals(firstName, person.getName());
+    assertEquals(firstName, personWithMiddleName.getName());
   }
-
-  @Test
-  void addChild() {
-  }
-
-  @Test
-  void addSibbling() {
-  }
-
-  @Test
-  void getGrandChildren() {
-  }
-
-  @Test
-  void getName() {
-  }
-
-  @Test
-  void getMiddleName() {
-  }
-
-  @Test
-  void getLastName() {
-  }
-
-  @Test
-  void getSex() {
-  }
-
-  @Test
-  void getAge() {
-  }
-
-  @Test
-  void getMother() {
-  }
-
-  @Test
-  void getFather() {
-  }
-
-  @Test
-  void getSiblings() {
-  }
-
-  @Test
-  void getChildren() {
-  }
-
-  can
 
 }
